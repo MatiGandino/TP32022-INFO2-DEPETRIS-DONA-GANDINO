b6b23c12196b6be78922ea572ede96c495c07b97
Rafael-Dona <69308972+Rafael-Dona@users.noreply.github.com>
2022-06-23 10:45:14 -0300

    Corrección Bugs

--- TP3_Informatica_c/TP3_Informatica_Zinjai.c
+++ TP3_Informatica_c/TP3_Informatica_Zinjai.c
@@ -24,39 +24,44 @@ union Umed {
 	byte a[sizeof(mediciones)];
 }medicion;
 
-
 int main(void) {
 	
-	int cport_nr = 5; //NUMERO DE PUERTO SERIE (verificar numero)
+	int cport_nr = 3; //NUMERO DE PUERTO SERIE (verificar numero)
 	int bdrate = 9600;
 	char mode[] = {'8', 'N', '1', 0};
 	char letra;
-
+	
 	if(RS232_OpenComport(cport_nr, bdrate, mode, 0)){
 		printf("No se puede abrir el puerto.\n");
 		return (0);}
 	
 	
-while(1){
-	printf("Intraduzca la inicial del sensor a leer: ");
-	scanf(" %c", &letra);
-	RS232_SendByte(cport_nr, letra);
+	while(1){
+		printf("Intraduzca la inicial del sensor a leer: ");
+		scanf(" %s", &letra);
+		RS232_SendByte(cport_nr, letra);
+		Sleep(1000);
+		
+		/*
+		if (RS232_SendByte(cport_nr, letra) == 1){
+			printf("ERROR!!!"); 
+		}	*/
+		
+		//Sleep(1000);
+		
+		
+			int cantidadBytes = RS232_PollComport(cport_nr, medicion.a, sizeof(mediciones));
+			
+			if (cantidadBytes > 0){
+				printf("El sensor es %c\n", medicion.med.indic);
+				printf("El valor es %d\n", medicion.med.valor);
+				printf("Tiempo desde la ultima medicion: %d\n\n", medicion.med.tiempo);
+				//cantidadBytes = 0;
+				//return 0;
+				}
+			
+		}
 
-	
-	if (RS232_SendByte(cport_nr, letra) == 1){
-		printf("ERROR!!!"); }
-	
-	Sleep(1000);
-	
-	int cantidadBytes = RS232_PollComport(cport_nr, medicion.a, sizeof(mediciones));
-	
-	if (cantidadBytes > 0){
-	printf("El sensor es %c\n", medicion.med.indic);
-	printf("El valor es %d\n", medicion.med.valor);
-	printf("Tiempo desde la ultima medicion: %d\n\n", medicion.med.tiempo);
-	cantidadBytes = 0;
-	}
-}
 
 return 0;
 }
