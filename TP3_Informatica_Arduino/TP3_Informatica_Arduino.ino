730032209336ca28c80f78835afe8700e5b45db7
NicoDepetris02 <101680079+NicoDepetris02@users.noreply.github.com>
2022-06-23 14:53:56 -0300

    Comentarios

--- TP3_Informatica_Arduino/TP3_Informatica_Arduino.ino
+++ TP3_Informatica_Arduino/TP3_Informatica_Arduino.ino
@@ -1,7 +1,7 @@
 #include <DHT.h>                            //Librerias humedad
 #include <DHT_U.h>                          //       "
 #include <stdint.h>                         //Libreria para usar uint16_t
-#define SENSORTEM A0                        //Variable de sensor de temperatura
+#define SENSORTEM A0                        //Definimos PIN de entrada analogica para la variable sensor de temperatura
 
 
 char letra;                                 //Char que recibe la letra del sensor
@@ -33,55 +33,55 @@ union Umed {                                //Union donde se guardaran las medic
                                           
 
 void setup(){
-  Serial.begin(9600);                       //Inicializar el monitor serie
+  Serial.begin(9600);                       //Inicializamos el puerto serie en 9600 Baudios
   
   dht.begin ();                             //Inicialización del sensor de humedad
   
   pinMode (TRIG, OUTPUT);                   //Salida ultrasonido
   pinMode (ECO, INPUT);                     //Entrada ultrasonido
   
-  tiempo0 = 0;                              //Inicializamos tiempo
+  tiempo0 = 0;                              //Inicializamos el referenciamiento del tiempo
 }
 
 
 void loop() {
-    if(Serial.available()){
-    letra = Serial.read();
+    if(Serial.available()){                 //Si existe una lectura de datos desde el puerto serie habilita lo que hay dentro, sino no
+    letra = Serial.read();                  //Realiza la lectura de lo que se envia por el puerto serie
     
   //Datos de sensor temperatura----------------------------------------------   
-      if (letra == 't' || letra == 'T'){
-      TEM = analogRead(SENSORTEM); 
-      int temperatura = (((TEM * 5000.0) / 1023)/10);                                  
-      temp.med.valor = temperatura;
-      temp.med.tiempo = millis() - tiempo0;
-      temp.med.indic = 'T'; 
+      if (letra == 't' || letra == 'T'){                                //Evalua la letra recibida
+      TEM = analogRead(SENSORTEM);                                      //Lee y guarda el valor del sensor de temperatura
+      int temperatura = (((TEM * 5000.0) / 1023)/10);                   //Convierte el valor del sensor y lo guarda en otra varialble                      
+      temp.med.valor = temperatura;                                     //Asigna el de "temperatura" a la variable de la estructura
+      temp.med.tiempo = millis() - tiempo0;                             //Define el tiempo desde la ultima medicion
+      temp.med.indic = 'T';                                             //Guarda el caracter al que indica el sensor
       for (unsigned long long i = 0; i < sizeof(mediciones); i++){
-        Serial.write (temp.a[i]); }
+        Serial.write (temp.a[i]); }                                     //Envia los datos guardados y compartidos en la union a traves de un arreglo
       }
   
   //Datos de sensor humedad--------------------------------------------------     
-      if (letra == 'h' || letra == 'H'){
-      uint16_t HUMEDAD = dht.readHumidity();  
-      hum.med.tiempo = millis() - tiempo0;
-      hum.med.valor = HUMEDAD;
-      hum.med.indic = 'H'; 
-      for (unsigned long long i = 0; i < sizeof(mediciones); i++){
-        Serial.write (hum.a[i]); }
+      if (letra == 'h' || letra == 'H'){                                //Evalua la letra recibida
+      uint16_t HUMEDAD = dht.readHumidity();                            //Lee y guarda el valor del sensor de humedad
+      hum.med.tiempo = millis() - tiempo0;                              //Define el tiempo desde la ultima medicion
+      hum.med.valor = HUMEDAD;                                          //Asigna el de "HUMEDAD" a la variable de la estructura
+      hum.med.indic = 'H';                                              //Guarda el caracter al que indica el sensor
+      for (unsigned long long i = 0; i < sizeof(mediciones); i++){          
+        Serial.write (hum.a[i]); }                                      //Envia los datos guardados y compartidos en la union a traves de un arreglo
       }
   
   //Datos de sensor ultrasonido----------------------------------------------
-      if (letra == 'u' || letra == 'U'){
-      digitalWrite(TRIG, HIGH);
-      delay (1);                                                              //Funcionamiento sensor ultrasonido
-      digitalWrite (TRIG, LOW);
+      if (letra == 'u' || letra == 'U'){                                      //Evalua la letra recibida
+      digitalWrite(TRIG, HIGH);                                               //Funcionamiento sensor ultrasonido
+      delay (1);                                                              //               "
+      digitalWrite (TRIG, LOW);                                               //               " 
       DURACION = pulseIn (ECO, HIGH);                                         //               "
-      DISTANCIA = DURACION / 58.2;
+      DISTANCIA = DURACION / 58.2;                                            //Asigna la medicion del sensor ultrasonido a la variable "DISTANCIA"
   
-      ult.med.valor = DISTANCIA;
-      ult.med.tiempo = millis() - tiempo0;
-      ult.med.indic = 'U';
+      ult.med.valor = DISTANCIA;                                              //Asigna el de "DISTANCIA" a la variable de la estructura                               
+      ult.med.tiempo = millis() - tiempo0;                                    //Define el tiempo desde la ultima medicion
+      ult.med.indic = 'U';                                                    //Guarda el caracter al que indica el sensor
       for (unsigned long long i = 0; i < sizeof(mediciones); i++){
-        Serial.write (ult.a[i]); }
+        Serial.write (ult.a[i]); }                                            //Envia los datos guardados y compartidos en la union a traves de un arreglo
       }
   
   //-------------------------------------------------------------------------
